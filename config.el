5a6f1bca42528cdd0c86f67416217f5caf2bf055
Juan Edi <jedi@Juans-MacBook-Pro.local>
2021-03-19 00:56:51 -0300

    keybindings for beginning and end of line

--- config.el
+++ config.el
@@ -52,3 +52,8 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(evil-define-key* 'normal 'global
+  (kbd "C-H") #'evil-first-non-blank
+  (kbd "C-L") #'evil-end-of-line
+  )
