5cf135666fc245f656a00190fea5ea02e6a4e0f6
Juan Edi <jedi@Juans-MacBook-Pro.local>
2021-03-21 03:18:18 -0300

    helpers for elm imports

--- config.el
+++ config.el
@@ -66,14 +66,50 @@
 
 (setq elm-format-on-save t)
 
+(defun elm/import (&optional input)
+  "Prompts for an import statement to add to the current file"
+  (interactive)
+  (let ((statement (read-string "Import statement: " (concat "import " input))))
+    (save-excursion
+      (goto-char (point-min))
+      (if (re-search-forward "^import " nil t)
+          (beginning-of-line)
+        (forward-line 1)
+        (insert "\n"))
+      (insert (concat statement "\n")))
+    (elm-sort-imports)))
+
+(defun elm/import-from-file ()
+  "Selects an elm file interactively and adds an import for the corresponding module"
+  (interactive)
+  (let*
+      ((all-files (projectile-current-project-files))
+       (elm-files (seq-filter (lambda (f) (s-ends-with-p ".elm" f)) all-files))
+       (file-name (projectile-completing-read "Module to import: " elm-files)))
+    (when file-name
+      (let*
+          ((full-file-name (expand-file-name file-name (projectile-project-root)))
+           (module-name (with-current-buffer (find-file-noselect full-file-name)
+                          (elm--get-module-name)))
+           (aliased-module-name (elm/aliased-module-name module-name)))
+        (elm/import aliased-module-name)))))
+
+(defun elm/aliased-module-name (module-name)
+  (let ((components (s-split "\\." module-name)))
+    (if (< 1 (length components))
+        (concat module-name " as " (car (last components)))
+        (concat module-name))))
+
 (map! :map elm-mode-map
       (:leader
+       (:prefix ("m i" . "import")
+        :desc "Add import"              "i"   #'elm/import
+        :desc "Add import from file"    "f"   #'elm/import-from-file)
        (:prefix ("m t" . "tests")
         :desc "Run tests in buffer"                    "b"   #'elm-test-runner-run
         :desc "Re-run last test"                       "r"   #'elm-test-runner-rerun
         :desc "Toggle between test and implementation" "TAB" #'elm-test-runner-toggle-test-and-target
         )))
-
 
 ;; -----------------------------------------------
 ;; Flycheck
