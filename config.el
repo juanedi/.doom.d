87e1502619e7fb8ea20d41265b486ccf07d6aa9e
Juan Edi <juan@noredink.com>
2022-10-25 23:34:09 -0300

    :art: tidy up a bit

--- config.el
+++ config.el
@@ -405,7 +405,17 @@
       "w s" #'+evil/window-split-and-follow
 
       "w x" #'kill-buffer-and-window
-      )
+
+      "0" #'winum-select-window-0-or-10
+      "1" #'winum-select-window-1
+      "2" #'winum-select-window-2
+      "3" #'winum-select-window-3
+      "4" #'winum-select-window-4
+      "5" #'winum-select-window-5
+      "6" #'winum-select-window-6
+      "7" #'winum-select-window-7
+      "8" #'winum-select-window-8
+      "9" #'winum-select-window-9)
 
 (use-package! tree-sitter
   :config
@@ -422,18 +432,6 @@
 (use-package! dhall-mode
   :ensure t
   :mode "\\.dhall\\'")
-
-(map! :leader
-  "0" #'winum-select-window-0-or-10
-  "1" #'winum-select-window-1
-  "2" #'winum-select-window-2
-  "3" #'winum-select-window-3
-  "4" #'winum-select-window-4
-  "5" #'winum-select-window-5
-  "6" #'winum-select-window-6
-  "7" #'winum-select-window-7
-  "8" #'winum-select-window-8
-  "9" #'winum-select-window-9)
 
 (use-package! ivy-posframe
   :config
