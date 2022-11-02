321e182b3889854f7ebfdb47a4038f7f89b11d9e
Juan Edi <juan@noredink.com>
2022-11-02 00:50:40 -0300

    exclude haskell-mode from tree sitter highlighting

--- config.el
+++ config.el
@@ -437,7 +437,7 @@
   :config
   (require 'tree-sitter-langs)
   (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+  (add-hook 'tree-sitter-after-on-hook (lambda () (unless (eq major-mode 'haskell-mode) (tree-sitter-hl-mode)))))
 
 (remove-hook! 'doom-modeline-mode-hook #'size-indication-mode)
 
