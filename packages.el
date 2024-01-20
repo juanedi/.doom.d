7808e41363289cd643c8755c363e64e7dc666aae
Juan Edi <juan@noredink.com>
2024-01-20 14:53:49 -0300

    drop all things copilot

--- packages.el
+++ packages.el
@@ -60,6 +60,3 @@
 (package! tree-sitter)
 (package! tree-sitter-langs)
 (package! graphviz-dot-mode)
-
-(package! copilot
-  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
