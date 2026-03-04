41945e5f705dd78a2d0b5cdd7119561df81f224c
Juan Edi <jedi11235@gmail.com>
2026-03-04 17:26:20 -0300

    winum keybindings

--- config.el
+++ config.el
@@ -353,15 +353,15 @@ corresponding module"
       "w s" #'+evil/window-split-and-follow
       "w x" #'kill-buffer-and-window
 
-      ;; "0" #'treemacs-select-window
-      ;; "1" #'winum-select-window-1
-      ;; "2" #'winum-select-window-2
-      ;; "3" #'winum-select-window-3
-      ;; "4" #'winum-select-window-4
-      ;; "5" #'winum-select-window-5
-      ;; "6" #'winum-select-window-6
-      ;; "7" #'winum-select-window-7
-      ;; "8" #'winum-select-window-8
-      ;; "9" #'winum-select-window-9
+      "0" #'treemacs-select-window
+      "1" #'winum-select-window-1
+      "2" #'winum-select-window-2
+      "3" #'winum-select-window-3
+      "4" #'winum-select-window-4
+      "5" #'winum-select-window-5
+      "6" #'winum-select-window-6
+      "7" #'winum-select-window-7
+      "8" #'winum-select-window-8
+      "9" #'winum-select-window-9
       )
 
